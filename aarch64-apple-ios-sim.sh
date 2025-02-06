250befcaeea4e092ccbe5c2d77008aca291b1380
hkarani <hezronkaranii@gmail.com>
2025-02-06 11:29:54 +0300

    Cargokit build for all ios targets

---
+++ aarch64-apple-ios-sim.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASE_PATH="$(pwd)"
+
+cd src/*/cargokit/build_tool/bin || { echo "Failed to change directory"; exit 1; }
+
+export IPHONEOS_DEPLOYMENT_TARGET=10.0
+exec env \
+  CARGOKIT_ROOT_PROJECT_DIR="$BASE_PATH/src/lwk-dart" \
+  CARGOKIT_DARWIN_ARCHS="arm64" \
+  CARGOKIT_CONFIGURATION="release" \
+  CARGOKIT_TARGET_TEMP_DIR="$BASE_PATH/lib/macos/aarch64-apple-ios-sim" \
+  CARGOKIT_DARWIN_PLATFORM_NAME="iphoneos" \
+  CARGOKIT_MANIFEST_DIR="$BASE_PATH/src/lwk-dart/rust" \
+  CARGOKIT_OUTPUT_DIR="$BASE_PATH/release/ios/aarch64-apple-ios-sim" \
+  dart run build_tool build-pod
+
+echo "Build completed"
---
+++ x86_64-apple-ios.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASE_PATH="$(pwd)"
+
+cd src/*/cargokit/build_tool/bin || { echo "Failed to change directory"; exit 1; }
+
+export IPHONEOS_DEPLOYMENT_TARGET=10.0
+exec env \
+  CARGOKIT_ROOT_PROJECT_DIR="$BASE_PATH/src/lwk-dart" \
+  CARGOKIT_DARWIN_ARCHS="x86_64" \
+  CARGOKIT_CONFIGURATION="release" \
+  CARGOKIT_TARGET_TEMP_DIR="$BASE_PATH/lib/macos/x86_64-apple-ios" \
+  CARGOKIT_DARWIN_PLATFORM_NAME="iphonesimulator" \
+  CARGOKIT_MANIFEST_DIR="$BASE_PATH/src/lwk-dart/rust" \
+  CARGOKIT_OUTPUT_DIR="$BASE_PATH/release/ios/x86_64-apple-ios" \
+  dart run build_tool build-pod
+
+echo "Build completed"
